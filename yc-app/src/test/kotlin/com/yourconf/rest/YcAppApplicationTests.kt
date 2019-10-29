26bb2326c97e47564c5f7437f28dcadd89f29ac0
Mike Chernev <mike@mikechernev.com>
2019-10-29 22:35:50 +0200

    Initial app

---
+++ yc-app/src/test/kotlin/com/yourconf/rest/YcAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourconf.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YcAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
